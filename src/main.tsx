6e26d2dc98e2940b76b97abd31da581aca6aedf8
harshkr040103 <harshkr040103@gmail.com>
2025-12-13 22:21:22 +0530

    Render App.jsx from main.tsx with React.StrictMode & BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
   
